8a9493030920760b7034b88a2ba313ef0d7a1dac
aa <36773044+aaqsr@users.noreply.github.com>
2021-07-02 22:37:33 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ if [[ $OSTYPE == "linux"* ]]; then
   if [ -f "/etc/arch-release" ]; then
   sudo pacman -Syyu nodejs npm
   
+  # dependencies
+   sudo pacman -S neovim # neovim cause neovim 
   
   # else assumes ubuntu
   else
@@ -24,6 +26,16 @@ fi
 
 sudo npm install -g dotfiles-sync
 dotfiles sync https://github.com/aaqsr/dotfiles.git
+
+# pulls dependencies
+npm install -g spaceship-prompt # for zsh prompt
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # neovim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # vim plug
+
+
+
 
 # put this in ur home
 # remember to set it to be executable by chmod +x
